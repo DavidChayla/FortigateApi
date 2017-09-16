70ea4f683f0885b9609673676867076f2b909f98
admiralspark <admiralspark@gmail.com>
2017-09-15 20:39:04 -0800

    Added support for packaging into PyPi. This will let people 'pip install FortigateApi'

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'FortigateApi',
+  packages = ['FortigateApi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Access Fortigate REST API in python',
+  author = 'David Chayla',
+  author_email = 'null',
+  url = 'https://github.com/DavidChayla/FortigateApi', 
+  download_url = 'https://github.com/DavidChayla/FortigateApi/archive/0.1.tar.gz', 
+  keywords = ['fortigate', 'REST', 'network'],
+  classifiers = [],
+)
